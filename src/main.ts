ea223494990132ebed2d9d675bbd98e02ccc6145
luka tskhomelidze <89034140+Boy753hub@users.noreply.github.com>
2025-12-27 19:31:32 +0400

    test version 8.4

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  mongoose.connection.on('connected', () =>
+    console.log('✅ Mongo connected:', mongoose.connection.name),
+  );
+  mongoose.connection.on('error', (err) => console.log('❌ Mongo error:', err));
+  mongoose.connection.on('disconnected', () =>
+    console.log('⚠️ Mongo disconnected'),
+  );
 }
 bootstrap();
