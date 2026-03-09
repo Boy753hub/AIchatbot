e041c615ce4681cd9db78b866ad19996614d2eb8
luka tskhomelidze <89034140+Boy753hub@users.noreply.github.com>
2026-03-09 15:17:32 +0400

    toggle bot2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://drouli.ge'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
