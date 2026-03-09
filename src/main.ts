4dbf1aaed35f321709f1273166e11b629abe0981
luka tskhomelidze <89034140+Boy753hub@users.noreply.github.com>
2026-03-09 15:42:20 +0400

    fixed errors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  await app.listen(process.env.PORT ?? 3000);
 
   mongoose.connection.on('connected', () =>
     console.log('✅ Mongo connected:', mongoose.connection.name),
