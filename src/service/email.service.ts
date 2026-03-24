52a19a6620386fa45dc4d52330616b76ad4594bd
luka tskhomelidze <89034140+Boy753hub@users.noreply.github.com>
2026-03-24 18:20:13 +0400

    added email configuration for website

--- src/service/email.service.ts
+++ src/service/email.service.ts
@@ -9,8 +9,8 @@ import * as nodemailer from 'nodemailer';
 export class EmailService {
   private transporter = nodemailer.createTransport({
     host: 'smtp.zoho.eu',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
