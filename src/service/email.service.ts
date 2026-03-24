a7583fd1399dc72aba6110fadf66c3d4a71e36ea
luka tskhomelidze <89034140+Boy753hub@users.noreply.github.com>
2026-03-24 18:27:41 +0400

    added email configuration for website

--- src/service/email.service.ts
+++ src/service/email.service.ts
@@ -15,6 +15,8 @@ export class EmailService {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    requireTLS: true,
+    connectionTimeout: 10000,
   });
 
   async sendContactEmail(data: any) {
